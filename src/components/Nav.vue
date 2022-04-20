b569bbd8ef9e00b7529931f7d080a586b41ceaef
Vitalii <98636866+VitaliiChernelivskyi@users.noreply.github.com>
2022-04-20 15:36:12 +0300

    add header

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <nav class="uk-navbar-container" uk-navbar>
-      <div class="uk-navbar-left">
-        <ul class="uk-navbar-nav">
-          <li>
-            <a href="/">NORDIC ROSE</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="uk-navbar-right">
-        <ul class="uk-navbar-nav">
-          <li v-for="category in categories.data" v-bind:key="category.id">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">NORDIC ROSE</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item nav-link active" v-for="category in categories.data" v-bind:key="category.id">
             <router-link
                 :to="{ path: '/category/' + category.id }"
                 :key="category.id"
@@ -22,13 +19,40 @@
         </ul>
       </div>
     </nav>
+
+<!--    <nav class="uk-navbar-container" uk-navbar>-->
+<!--      <div class="uk-navbar-left">-->
+<!--        <ul class="uk-navbar-nav">-->
+<!--          <li>-->
+<!--            <a href="/">NORDIC ROSE</a>-->
+<!--          </li>-->
+<!--        </ul>-->
+<!--      </div>-->
+
+<!--      <div class="uk-navbar-right">-->
+<!--        <ul class="uk-navbar-nav">-->
+
+<!--        </ul>-->
+<!--      </div>-->
+<!--    </nav>-->
   </div>
 </template>
 
-<style>
-a.router-link-active {
-  background: red;
-  border: black 1px !important;
+<style scoped>
+.navbar{
+  color:#E6E6E6 ;
+  border-bottom: 1px solid;
+}
+.navbar-collapse{
+  justify-content: flex-end;
+}
+.router-link-exact-active {
+  border-bottom: 4px solid;
+  padding-bottom: 20px;
+;
+}
+.nav-link{
+font-size: 20px
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import VueApollo from "vue-apollo";
 import apolloClient from "./vue-apollo";
 import VueRouter from "vue-router";
 import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
 
 
 import App from "./App.vue";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap.min'
 import './assets/css/main.css'
 
 Vue.config.productionTip = false;
@@ -18,29 +19,29 @@ Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
 const apolloProvider = new VueApollo({
-  defaultClient: apolloClient
+    defaultClient: apolloClient
 });
 
 const router = new VueRouter({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      components: require("./containers/Articles.vue")
-    },
-    {
-      path: "/article/:id",
-      components: require("./containers/Article.vue")
-    },
-    {
-      path: "/category/:id",
-      components: require("./containers/Category.vue")
-    }
-  ]
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            components: require("./containers/Articles.vue")
+        },
+        {
+            path: "/article/:id",
+            components: require("./containers/Article.vue")
+        },
+        {
+            path: "/category/:id",
+            components: require("./containers/Category.vue")
+        }
+    ]
 });
 
 new Vue({
-  apolloProvider,
-  router,
-  render: h => h(App)
+    apolloProvider,
+    router,
+    render: h => h(App)
 }).$mount("#app");
